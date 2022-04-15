938c6271b9f5a182c58d8202051c6ea17de9a8b4
keltdeep <keltdeep@gmail.com>
2022-04-15 14:07:02 +0300

    fixPipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
 import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import {ValidationPipe} from "./validations/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,9 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
   });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3001);
 }
 bootstrap();
